e093ee23728d9646a4b38238b998b37681696903
HenokG <hhenok1987@gmail.com>
2019-09-10 17:54:46 +0300

    fix bug causing NullPointerException under the Music Repository pattern usage

---
+++ README.md
@@ -0,0 +1,26 @@
+# listening-party
+
+An android native app that lets users listen(in groups), share and publish music
+
+## How Does It Look?
+
+### LOGIN
+
+![ScreenShot](./ScreenShots/1.PNG)
+
+## SIGNUP
+
+![ScreenShot](./ScreenShots/2.PNG)
+
+### MUSIC
+
+![ScreenShot](./ScreenShots/3.PNG)
+![ScreenShot](./ScreenShots/4.PNG)
+
+### Music Player
+
+![ScreenShot](./ScreenShots/5.PNG)
+
+### Video Player
+
+![ScreenShot](./ScreenShots/6.PNG)
--- app/src/main/java/com/listeningparty/listeningparty/ui/MainActivity.java
+++ app/src/main/java/com/listeningparty/listeningparty/ui/MainActivity.java
@@ -1,82 +1,75 @@
 package com.listeningparty.listeningparty.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.listeningparty.listeningparty.R;
 
+import androidx.navigation.Navigation;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, MusicPlayerFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener, ChatFragment.FromChatFragmentToMainActivityInteractionListener {
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.humberger_icon)
+    ImageView humbergerIcon;
+    @BindView(R.id.navigation)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        getFragmentManager().beginTransaction()
-                .add(R.id.fragment_player, AuthenticationFragment.newInstance())
-                .commit();
+        humbergerIcon.setOnClickListener(v -> {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, null, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            if (item.getTitle().equals("Chat")) {
+                Navigation.findNavController(MainActivity.this, R.id.my_nav_host_fragment).navigate(R.id.action_feedFragment_to_chatsFragment);
+            } else if (item.getTitle().equals("For You")) {
+                Navigation.findNavController(MainActivity.this, R.id.my_nav_host_fragment).navigate(R.id.action_chatsFragment_to_feedFragment);
+            }
+            return true;
+        });
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -85,22 +78,27 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_profile) {
+            Navigation.findNavController(this, R.id.my_nav_host_fragment).navigate(R.id.action_feedFragment_to_profileFragment);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onMusicPlayerOpened(boolean isOpened) {
+        bottomNavigationView.setVisibility(isOpened ? View.INVISIBLE : View.VISIBLE);
+    }
+
+    @Override
+    public void onProfileFragmentOpened(boolean shouldHideBottomNavigation) {
+        bottomNavigationView.setVisibility(shouldHideBottomNavigation ? View.INVISIBLE : View.VISIBLE);
+    }
+
+    @Override
+    public void onChatOpened(boolean isOpened) {
+        bottomNavigationView.setVisibility(isOpened ? View.INVISIBLE : View.VISIBLE);
+        humbergerIcon.setVisibility(isOpened ? View.INVISIBLE : View.VISIBLE);
     }
 }
